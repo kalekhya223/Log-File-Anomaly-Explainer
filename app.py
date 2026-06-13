129cbd9ab52eeadcb425ba4b21d3904c0c7e7608
Kattamuri Alekhya <kalekhya223@gmail.com>
2026-06-13 14:57:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -519,5 +519,8 @@ def delete_history(result_id):
         flash(f'Error deleting report: {str(e)}', 'danger')
         return redirect(url_for('index'))
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
